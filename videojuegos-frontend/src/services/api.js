79b8fcda887117b42600dd1fb2c7ebdc5ce0d26d
AndresEstrada10 <e1351924046@live.uleam.edu.ec>
2024-12-04 11:00:00 -0500

    agregacion de api.js

--- videojuegos-frontend/src/services/api.js
+++ videojuegos-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Dirección del backend
+  baseURL: "https://videojuego-pass-2.onrender.com" // Dirección del backend
 });
 
 // Endpoints de videojuegos
