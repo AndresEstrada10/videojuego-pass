e1f5fafed73d52482c8a458d57aa30500f9a2ad9
AndresEstrada10 <e1351924046@live.uleam.edu.ec>
2024-12-04 15:32:33 -0500

    actulizacion

--- videojuegos-frontend/src/services/api.js
+++ videojuegos-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Dirección del backend
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000', // Dirección del backend
 });
 
 // Endpoints de videojuegos
