46baa59bf1eaa59883136afb5f3786aad0761958
AndresEstrada10 <e1351924046@live.uleam.edu.ec>
2024-12-04 11:08:17 -0500

    agregacion de babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+      'react-app', // Otras configuraciones predeterminadas de Create React App
+    ],
+    plugins: [
+      '@babel/plugin-proposal-private-property-in-object', // Plugin recomendado para solucionar el error
+    ],
+  };
+  
