137ad9a3ed41b2f746bff477e60646abf52861e5
AndresEstrada10 <139491428+AndresEstrada10@users.noreply.github.com>
2024-12-05 04:20:06 -0500

    Update api.js

--- videojuegos-frontend/src/services/api.js
+++ videojuegos-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000', // Dirección del backend
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'https://videojuego-pass.onrender.com', // Dirección del backend
   timeout: 10000,   
 });
 // Endpoints de videojuegos
